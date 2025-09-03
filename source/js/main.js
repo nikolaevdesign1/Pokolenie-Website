81731a36a51ccdc5e7dde5f9555cf56f06ad1886
Oleg Nikolaev <olegnikolaev@192.168.1.3>
2025-09-03 21:25:01 +0300

    Обновление 03.09.2025

--- source/js/main.js
+++ source/js/main.js
@@ -14,3 +14,36 @@ $('.section4-logos-slider').slick({
     slidesToShow:7.3,
     
 })
+
+
+$('.section7-slider').slick({
+    speed: 600,
+    autoplay:true,
+    arrows: false,
+    slidesToShow:4.2,
+    
+})
+
+
+$('.section-students-slider-slider').slick({
+    arrows: true,
+    dots:true,
+    infinite:true,
+    centerMode: true,
+    slidesToShow:4,
+})
+
+
+  //faq start
+
+  let questionTitle = document.querySelectorAll('.question');
+  
+  let toggleQuestion = function(){
+    this.classList.toggle('active');
+  }
+
+  questionTitle.forEach(function(title){
+    title.addEventListener('click', toggleQuestion);
+  })
+
+  //faq end
