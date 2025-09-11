4e3be68f853fc7d47297e76200a83f2a1911959b
Oleg Nikolaev <olegnikolaev@192.168.1.3>
2025-09-11 20:06:00 +0300

    Обновление 11.09.2025

--- source/js/main.js
+++ source/js/main.js
@@ -59,3 +59,61 @@ arrows: true,
   })
 
   //faq end
+
+
+
+
+  //popups start
+
+  $('.start-registration').on('click',function(){
+    $('.popup-years').toggleClass("active");
+  });
+
+  $('.popup-background').on('click',function(){
+    $('.popup-years').removeClass("active");
+    $('.sign-in, .less, .more, .popup-years, .pass1, .pass2, .pass3').removeClass("active");
+  });
+
+  $('.popup-content-buttons p:nth-child(1)').on('click',function(){
+    $('.more').toggleClass("active");
+    $('.popup-years').removeClass("active");
+  });
+
+  $('.popup-content-buttons p:nth-child(2)').on('click',function(){
+    $('.less').toggleClass("active");
+    $('.popup-years').removeClass("active");
+  });
+
+  $('.sign-in-button').on('click',function(){
+    $('.sign-in').toggleClass("active");
+    $('.popup-years, .less, .more, .thanks, .pass1, .pass2, .pass3').removeClass("active");
+  });
+
+  $('.registration_button').on('click',function(){
+    $('.popup-years').toggleClass("active");
+    $('.sign-in, .less, .more, .pass1, .pass2, .pass3').removeClass("active");
+  });
+
+  $('.thanks_button').on('click',function(){
+    $('.thanks').toggleClass("active");
+    $('.sign-in, .less, .more, .popup-years, .pass1, .pass2, .pass3').removeClass("active");
+  });
+  
+
+ $('.forgot-pass').on('click',function(){
+    $('.pass1').toggleClass("active");
+    $('.sign-in, .less, .more, .popup-years, .pass2, .pass3').removeClass("active");
+  });
+
+
+ $('.pass1 button').on('click',function(){
+  $('.pass2').toggleClass("active");
+  $('.sign-in, .less, .more, .popup-years, .pass1, .pass3').removeClass("active");
+});
+
+$('.go_pass3').on('click',function(){
+  $('.pass3').toggleClass("active");
+  $('.sign-in, .less, .more, .popup-years, .pass1, .pass2').removeClass("active");
+});
+
+  //popups finish
